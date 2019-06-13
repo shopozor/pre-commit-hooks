86cb61ed287aa3f095e38fde69368a4ae1b79284
Laurent Michel <laurent.michel@softozor.ch>
2019-06-13 18:06:48 +0200

    implement feature tags removal

--- hooks/check_feature_tags.py
+++ hooks/check_feature_tags.py
@@ -2,6 +2,24 @@ import argparse
 import io
 import sys
 
+def remove_unwanted_tags(filename):
+    # infile = "messy_data_file.txt"
+    # outfile = "cleaned_file.txt"
+
+    # delete_list = ["word_1", "word_2", "word_n"]
+    # fin = open(infile)
+    # fout = open(outfile, "w+")
+    # for line in fin:
+    #     for word in delete_list:
+    #         line = line.replace(word, "")
+    #     fout.write(line)
+    # fin.close()
+    # fout.close()
+    with open(filename, 'r') as file:
+        data = file.read().replace('@current', '')
+    
+    with open(filename, 'w') as file:
+        file.write(data)
 
 def main(argv=None):  # type: (Optional[Sequence[str]]) -> int
     parser = argparse.ArgumentParser()
@@ -11,6 +29,7 @@ def main(argv=None):  # type: (Optional[Sequence[str]]) -> int
     retval = 0
     for filename in args.filenames:
         print('filename: ', filename)
+        remove_unwanted_tags(filename)
         # try:
         #     with io.open(filename, 'rb') as xml_file:
         #         xml.sax.parse(xml_file, xml.sax.handler.ContentHandler())
