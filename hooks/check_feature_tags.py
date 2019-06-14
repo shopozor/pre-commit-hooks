d0eec8822a192028549e925435b4e0eb91e0b0f5
Laurent Michel <laurent.michel@softozor.ch>
2019-06-14 08:11:28 +0200

    implement check_feature_tags

--- hooks/check_feature_tags.py
+++ hooks/check_feature_tags.py
@@ -3,7 +3,7 @@ import re
 import sys
 
 def remove_unwanted_tags(filename):
-    with open(filename, "rw") as file:
+    with open(filename, "r+") as file:
         data = re.sub(r'@(current|focus)', '', file.read())
     
     # with open(filename, 'w') as file:
